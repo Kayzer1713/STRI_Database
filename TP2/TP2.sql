7698c600c7a2a9470f17b2c5db00293b83ae9d7b
Kayzer1713 <ghislain.vidal1@gmail.com>
2017-09-08 12:26:48 +0200

    Recherches Q1

--- TP2/TP2.sql
+++ TP2/TP2.sql
@@ -20,22 +20,34 @@ FROM Concert;
 DBMS_output.put_line('Nombre de concert(s) enregistré(s) dans la table: ' || vNbConcert);
 
 -- Obtention de l'idArtiste pour travailler plus simplement
+
 SELECT idArtiste INTO vIdArtiste
 FROM Artiste
 WHERE prenomArtiste = pPrenomArtiste
   AND nomArtiste = pNomArtiste;
 
 -- Création du concert
+
 INSERT INTO Concert
 VALUES (pIdConcert,
         pNomConcert,
         pLieuConcert,
         pDateConcert);
 
-FOR c1_ligne IN (SELECT idChanson FROM Chanson, Artiste WHERE )
-LOOP
 
-END LOOP;
+FOR c1_ligne IN (
+SELECT *
+FROM Artiste,
+     Interpreter,
+     Piste,
+     Album
+WHERE Artiste.idArtiste = vIdArtiste
+  AND Interpreter.idArtiste = Artiste.idArtiste
+  AND Interpreter.idPiste = Piste.idPiste
+  AND Piste.idAlbum = Album.idAlbum
+  AND ANNEEALBUM < pDateConcert;
+
+) LOOP END LOOP;
 -- récupèrer l'année d'une date: to_number(to_char(to_date(‘JJ/MM/YYYY’),’YYYY’))
 
 END; /
@@ -47,4 +59,5 @@ WHERE Artiste.idArtiste = 1
 AND Interpreter.idArtiste = Artiste.idArtiste
 AND Interpreter.idPiste = Piste.idPiste
 AND Piste.idAlbum = Album.idAlbum
-AND anneeAlbum.to_number(to_char(to_date(‘JJ/MM/YYYY’),’YYYY’)) < to_number(2016);
+AND EXTRACT(YEAR FROM
+   TO_DATE('1998-03-07', 'DD-MON-RR')) < 2010;
