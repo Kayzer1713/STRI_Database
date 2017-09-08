6b086d7400995417a20a7f7f2d1d702faca43c1e
Kayzer1713 <ghislain.vidal1@gmail.com>
2017-09-08 11:39:22 +0200

    Sujet + début Q1 TP2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 STRI database practical work
 
 ## TP1: PL/SQL
+
+## TP2 Procédures PL/SQL : gestion des curseurs
---
+++ TP2/TP2.sql
@@ -0,0 +1,50 @@
+-- TP2 Procédures PL/SQL : gestion des curseurs --
+
+SET SERVEROUTPUT ON;
+
+ -- Q1
+CREATE OR REPLACE PROCEDURE ajoutConcert (pIdConcert Concert.idConcert%TYPE,
+                            pNomConcert Concert.nomConcert%TYPE,
+                            pLieuConcert Concert.lieuConcert%TYPE,
+                            pDateConcert Concert.dateConcert%TYPE,
+                            pNomArtiste Artiste.nomArtiste%TYPE,
+                            pPrenomArtiste Artiste.prenomArtiste%TYPE) AS
+vNbConcert NUMBER:=0;
+vIdArtiste Artiste.idArtiste%TYPE;
+
+BEGIN
+
+SELECT count(*) INTO vNbConcert
+FROM Concert;
+
+DBMS_output.put_line('Nombre de concert(s) enregistré(s) dans la table: ' || vNbConcert);
+
+-- Obtention de l'idArtiste pour travailler plus simplement
+SELECT idArtiste INTO vIdArtiste
+FROM Artiste
+WHERE prenomArtiste = pPrenomArtiste
+  AND nomArtiste = pNomArtiste;
+
+-- Création du concert
+INSERT INTO Concert
+VALUES (pIdConcert,
+        pNomConcert,
+        pLieuConcert,
+        pDateConcert);
+
+FOR c1_ligne IN (SELECT idChanson FROM Chanson, Artiste WHERE )
+LOOP
+
+END LOOP;
+-- récupèrer l'année d'une date: to_number(to_char(to_date(‘JJ/MM/YYYY’),’YYYY’))
+
+END; /
+
+
+SELECT *
+FROM Artiste, Interpreter, Piste, Album
+WHERE Artiste.idArtiste = 1
+AND Interpreter.idArtiste = Artiste.idArtiste
+AND Interpreter.idPiste = Piste.idPiste
+AND Piste.idAlbum = Album.idAlbum
+AND anneeAlbum.to_number(to_char(to_date(‘JJ/MM/YYYY’),’YYYY’)) < to_number(2016);
